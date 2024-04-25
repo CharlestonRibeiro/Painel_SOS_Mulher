c7bd8216f4d26ef5d07569f6e24c5f590e19e57f
unknown <charleston.passos@gmail.com>
2024-04-25 19:56:12 -0400

    feat - modular

---
+++ lib/app/painel_sos_mulher.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class PainelSosMulher extends StatelessWidget {
+  const PainelSosMulher({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Painel SOS Mulher',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+       routerConfig: Modular.routerConfig,
+    );
+  }
+}
