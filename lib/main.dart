38a304b6c62ed16ca3495b482173f47be8509537
unknown <charleston.passos@gmail.com>
2024-05-05 08:50:19 -0400

    feat - firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:painel_sos_mulher/app/app_modular.dart';
+import 'package:painel_sos_mulher/firebase_options.dart';
 import 'app/painel_sos_mulher.dart';
 
-void main() {
-  runApp(
-     ModularApp(
-        module: AppModule(), 
-        child: const PainelSosMulher()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(ModularApp(
+    module: AppModule(),
+    child: const PainelSosMulher(),
+  ));
 }
 
