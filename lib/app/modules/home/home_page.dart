f109f22134d97e552eae2d401bbab5bb277c7ff5
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2024-05-18 21:14:59 -0300

    fix: load - initialize controller

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -11,6 +11,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    _controller.load();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Painel SOS'),
