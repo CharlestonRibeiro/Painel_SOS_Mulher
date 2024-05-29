021cec3b30ddb254ec0f43ed1bba1e2847fca510
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2024-05-29 14:22:03 -0300

    feat: modelling audios

---
+++ lib/app/models/audio_model.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'model_errors.dart';
+
+final class Audio {
+  final String id;
+  final double latitude;
+  final double longitude;
+  final String url;
+  final DateTime time;
+
+  Audio({
+    required this.id,
+    required this.latitude,
+    required this.longitude,
+    required this.url,
+    required this.time,
+  });
+
+  factory Audio.fromMap(Map<String, dynamic> map) {
+    return switch (map) {
+      {
+        'id': String id,
+        'latitude': double lat,
+        'longitude': double lng,
+        'url': String url,
+        'time': Timestamp time,
+      } =>
+        Audio(
+          id: id,
+          latitude: lat,
+          longitude: lng,
+          url: url,
+          time: time.toDate(),
+        ),
+      {
+        'id': String id,
+        'url': String url,
+        'time': String time,
+      } =>
+        Audio(
+          id: id,
+          latitude: 0,
+          longitude: 0,
+          url: url,
+          time: DateTime.parse(time.replaceAll('.wav', '')),
+        ),
+      _ => throw DataFormatError(
+          map.toString(),
+          "{'id': String id, 'latitude': double lat, 'longitude': double lng, 'file': File file, 'time': Timestamp time}",
+        ),
+    };
+  }
+}
