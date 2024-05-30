ab40fcc892087e71a6f1ea43fd53c3e1fd0838f0
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2024-05-30 13:44:29 -0300

    fix: initial loading state management

--- lib/app/modules/audio/audio_states.dart
+++ lib/app/modules/audio/audio_states.dart
@@ -2,6 +2,8 @@ import '../../core/errors/app_error_interface.dart';
 
 sealed class AudioState {}
 
+class InitialAudioState implements AudioState {}
+
 class LoadingAudioState implements AudioState {}
 
 class SuccessAudioState implements AudioState {}
