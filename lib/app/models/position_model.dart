571f510a06d8b56077d0f34e6fc02bdb47fef54d
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2024-06-18 08:42:58 -0300

    feat: add name field to position model

--- lib/app/models/position_model.dart
+++ lib/app/models/position_model.dart
@@ -5,31 +5,37 @@ final class Position {
   final double latitude;
   final double longitude;
   final bool alert;
+  final String name;
 
   Position({
     required this.id,
     required this.latitude,
     required this.longitude,
     required this.alert,
+    required this.name,
   });
 
   factory Position.empty() =>
-      Position(id: '', latitude: 0, longitude: 0, alert: false);
+      Position(id: '', latitude: 0, longitude: 0, alert: false, name: '');
 
   factory Position.fromMap(Map<String, dynamic> map) {
     return switch (map) {
+      // Cenário ideal
       {
         'id': String id,
         'latitude': double lat,
         'longitude': double lng,
         'alert': bool alert,
+        'name': String name,
       } =>
         Position(
           id: id,
           latitude: lat,
           longitude: lng,
           alert: alert,
+          name: name,
         ),
+      // Cenário atual
       {
         'id': String id,
         'latitude': double lat,
@@ -40,7 +46,9 @@ final class Position {
           latitude: lat,
           longitude: lng,
           alert: false,
+          name: '',
         ),
+      // Dados em formato incorreto
       _ => throw DataFormatError(map.toString(),
           "{'id': String, 'latitude':double, 'longitude': double}"),
     };
