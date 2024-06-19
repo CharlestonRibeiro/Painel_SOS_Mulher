323a1cef1502637ba470c118e5ac56c105efc8a1
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2024-06-19 16:04:31 -0300

    refactor: remove unused history module

--- lib/app/core/routes/destinations.dart
+++ lib/app/core/routes/destinations.dart
@@ -5,6 +5,5 @@ import 'routes.dart';
 final destinations = [
   (Icons.my_location, Routes.home),
   (Icons.volume_up, Routes.audios),
-  (Icons.history, Routes.history),
   (Icons.person_add, Routes.register),
 ];
